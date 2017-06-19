606a88e8cd2d9ca42f9a47c0cf9f216c3212c3b2
Raghu Mulukutla <raghu.tjm@gmail.com>
2017-06-19 13:11:50 -0400

    First Commit
    Changed module name from “app“ to “mobile”
    Added a new wearable module “wear”

---
+++ device_usage/build/generated/source/buildConfig/release/com/aware/plugin/device_usage/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.aware.plugin.device_usage;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.aware.plugin.device_usage";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 19;
+  public static final String VERSION_NAME = "2.5";
+}
