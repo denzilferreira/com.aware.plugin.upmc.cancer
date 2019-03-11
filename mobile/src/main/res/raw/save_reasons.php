3251b30ec47e433ff5b81fb9619380724c4c424f
yiyir <yiyir@andrew.cmu.edu>
2019-03-11 17:34:28 -0400

    finalizedphp scripts

--- mobile/src/main/res/raw/save_reasons.php
+++ mobile/src/main/res/raw/save_reasons.php
@@ -5,11 +5,18 @@ $dataArray = json_decode($body,true);
 $unixTime = $dataArray['timeStamp'];
 $sessionId = $dataArray['sessionId'];
 $reasons = $dataArray['reasons'];
+$busy = substr($reasons,-5,1);
+$pain = substr($reasons,-4,1);
+$nausea = substr($reasons,-3,1);
+$tired = substr($reasons,-2,1);
+$other = substr($reasons,-1,1);
+echo "Reasons are: " . $reasons . "<br>";
 $conn = new PDO('mysql:dbname=UPMC;host=127.0.0.1','root','');
-
-$result = $conn->exec("INSERT INTO responses_watch(timestamp,session_id,busy,pain,nausea,tired,other) VALUES('$unixTime','$sessionId',$reasons{0},$reasons{1},$reasons{2},$reasons{3},$reasons{4})");
-if($result) {
-	echo $body;
+try{
+    $result = $conn->exec("INSERT INTO responses_watch(timestamp,session_id,busy,pain,nausea,tired,other) VALUES('$unixTime','$sessionId','$busy','$pain','$nausea','$tired','$other')");
+	echo "Result is: " .$result . "<br>";
+} catch (Exception $e) {
+    echo $sql . "<br>" . $e->getMessage();
 }
 $conn = null;
 ?>
